87565f4814f36ec6a208ed273f3db9d7aa3dc2e6
confident-token-1019 <theoonetj@gmail.com>
2020-01-21 03:22:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,43 +1,25 @@
-require_relative '../lib/turn'
-
-def input_to_index(input)
-  input.to_i - 1
-end 
-
-#if index is valid...
-def valid_move?(number_entered, board)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
-end
-#if index is valid...make the move for index
-def move(array, index, name = "X")
-  array[index] = name
-end
-#if index is valid...show the board
-def position_taken?(board, answer)
-  board[answer] != " " 
-end
-
-def display_board(board)
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-   puts "-----------"
-   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-   puts "-----------"
-   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-#this method need to be developed ________________
-def turn(board)
-    puts "Please enter 1-9:"
-    answer = gets.chomp
-    answer = input_to_index(answer)
-    if valid_move?(answer, board)
-      move(board, answer)
-      puts display_board(board)
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
     else
-      puts "That is an invalid entry!"
-      turn(board)
-    end 
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
 end
-
-
-board = ["X"," "," "," "," "," "," "," "," "]
